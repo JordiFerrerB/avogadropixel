4ec5471c407c58d09c0e7c7ccd9eb36f460acef8
JordiFerrerB <30603442+JordiFerrerB@users.noreply.github.com>
2018-04-05 17:24:02 +0200

    new animations

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-$(document).ready(function(){
-	$('.headB').on('click', function(){
-		$('html, body').animate({
-          scrollTop: $('#squareContainer').offset().top 
-        }, 1800)
+window.jQuery(function ($) {
+	$("header").css("margin-bottom", $(window).height());
+	$("body").css("height", $(window).height()*2);
+
+	//Header first click action
+	$(".continue-button:first").on('click', function(){
+		$("#square").removeClass("hidden");
+		$("section:first").removeClass("hidden");
+		$("section:first").addClass("first");
+		$('html, body').animate({scrollTop : $('#square').offset().top},900);
 	});
 
-	$('.grow1m').on('click', function(event){
-		event.preventDefault();
-		$('.square').css({'width': '1000px', 'height': '1000px'});
-		$('#squareContainer').css({'height': '1000px'});
-		$('.firstText').fadeOut(500, 'swing');
-		$('#secondStage').css({'display' : 'flex'});
-		$('html, body').animate({
-          scrollTop: $('#secondStage').offset().top
-        }, 3000)
+	//Second click actions
+	$('.second-button').on('click', function(){
+		$('.first').fadeOut();
+		$('#square').css('width', 1000);
+		$('#square').css('height', 1000);
+		$('.second-section').removeClass("hidden");
+		$("body").css("height", ($(window).height()*2) + 1300);
+		$('html, body').animate({scrollTop : $('.last-button').offset().top+350},1000);
+	});
+
+	//Last button actions
+	$('.last-button').on('click', function(){
+		$('.second-section').fadeOut();
+		$('.last-section').removeClass("hidden");
+		$('#square').css('margin-left', 15);
+		$('#square').css('width', 776015463763);
+		$('#square').css('height', 776015463763);
 	});
 });
